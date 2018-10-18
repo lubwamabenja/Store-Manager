15190c809cd2250128b5b7fbb126135872e7a02c
lubwamabenja <lubwama73@gmail.com>
2018-10-18 15:49:04 +0300

    organising python files in directories

--- api/venv/bin/activate.fish
+++ api/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/lubwama/Store-Manager/api/venv"
+set -gx VIRTUAL_ENV "/home/lubwama/project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
